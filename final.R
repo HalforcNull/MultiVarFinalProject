36468befbd526ebd96ba3c2b093f7ce127440ed3
HalforcNull <yaorunan@gmail.com>
2018-04-29 21:02:52 -0500

    init commit

---
+++ final.R
@@ -0,0 +1,29 @@
+# load training data
+load("~/GitHub/MultiVarFinalProject/STAT715_ink_data_training.rData")
+# setup work directiry
+setwd("~/GitHub/MultiVarFinalProject")
+
+
+head(ink.training.dat)
+
+# The data is a 4 dim table which is not same as what we had
+# we need 'down degree' it into a 2 dim table, first dim is observations(22*13), second is label and data(1+200*31)
+my.table <- NULL
+for(i in 1:13){
+  for(j in 1:22){
+    rawMatrix <- ink.training.dat[,,j,i]
+    rawVector <- as.vector(rawMatrix)
+    vectorWithLabel <- c(i,rawVector)
+    my.table <- cbind(my.table, vectorWithLabel)
+  }
+}
+colnames(my.table) <- NULL
+dim(my.table) 
+my.table<-t(my.table)
+my.table[,1]
+
+# Now we have table as what we using during class
+
+
+
+
